d519afac0dbf50b931ea58a96960be4a43c21383
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-06-09 19:58:00 +0200

    hashing function

--- chains.c
+++ chains.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <openssl/sha.h>
 
+int hash(uint8_t *input, size_t n, uint8_t *output)
+{
+  SHA1(input, n, output);
+}
 int reduction(uint8_t *input, size_t n, uint8_t *output, size_t length)
 {
   //get first length character of input
