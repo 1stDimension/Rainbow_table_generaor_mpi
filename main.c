26e6f03654837fa49d296aa5b3d8b15a5b676474
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-06-10 02:27:14 +0200

    adding function taken from stackoverflow

--- main.c
+++ main.c
@@ -15,6 +15,20 @@
 // new_hash_value
 // reduce_value
 // new_reduce_value
+int ipow(int base, int exp)
+{
+  int result = 1;
+  for (;;)
+  {
+    if (exp & 1)
+      result *= base;
+    exp >>= 1;
+    if (!exp)
+      break;
+    base *= base;
+  }
+  return result;
+}
 
 int main()
 {
