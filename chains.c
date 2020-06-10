e8a04cd7f9c610525955bdb6469c1222549c4b79
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-06-10 02:07:21 +0200

    Changing reduction algorytm

--- chains.c
+++ chains.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <openssl/sha.h>
 
-int hash(uint8_t *input, size_t n, uint8_t *output)
+void hash(uint8_t *input, size_t n, uint8_t *output)
 {
   SHA1(input, n, output);
 }
-int reduction(uint8_t *input, size_t n, uint8_t *output, size_t length)
+void reduction(uint8_t *input, size_t n, uint8_t *output, size_t length)
 {
   //get first length character of input
-  // memcpy();
+  // for (int i = 0; i < length; i++)
+  // {
+  // sprintf(output + 2 * i, "%x", input[i]);
+  // }
+  //take length first bytes of input
+  //divide by 2 then for each bite make bitwise and with 128
+  memcpy(output, input, length);
   for (int i = 0; i < length; i++)
   {
-    sprintf(output + 2 * i, "%x", input[i]);
+    output[i] %= 95;
+    output[i] += 32;
   }
-  // char *format = "%2x";
-  // sprintf(output, "");
 }
