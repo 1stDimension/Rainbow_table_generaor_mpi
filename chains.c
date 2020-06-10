6b703f193e4dd5b4fbad7e401f71fafc09f90de4
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-06-10 02:25:54 +0200

    Updating comments of reduction

--- chains.c
+++ chains.c
@@ -7,15 +7,14 @@ void hash(uint8_t *input, size_t n, uint8_t *output)
 {
   SHA1(input, n, output);
 }
+
+// input and output should be pre allocated
 void reduction(uint8_t *input, size_t n, uint8_t *output, size_t length)
 {
   //get first length character of input
-  // for (int i = 0; i < length; i++)
-  // {
-  // sprintf(output + 2 * i, "%x", input[i]);
-  // }
   //take length first bytes of input
   //divide by 2 then for each bite make bitwise and with 128
+  //needs error checking
   memcpy(output, input, length);
   for (int i = 0; i < length; i++)
   {
