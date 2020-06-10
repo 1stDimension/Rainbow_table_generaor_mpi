4a4e338a45c6e7f3522a7aed879c057b1245178c
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-06-10 11:30:55 +0200

    extracting to function

--- main.c
+++ main.c
@@ -30,16 +30,66 @@ int ipow(int base, int exp)
   return result;
 }
 
+int chain(int length)
+{
+}
+
+//if returns != there was overflow
+// beggining and next should be pre-alocated
+int next(int step, int min, int max, int range, int remainder, int times, char *beggining, char *next)
+{
+  int max_step = range - 1;
+  for (int i = 0; i < length; i++)
+  {
+    int power = ipow(range, i + 1);
+    int tmp = step % power;
+    char this_step = tmp / ipow(range, i);
+    this_step += remainder;
+    remainder = 0;
+    char test = begining[length - i - 1] + this_step;
+    // potensiall overflow problem
+    //Max step is also the bigest possible value in one column
+    if (test > (max_step + min) || test < begining[length - i - 1])
+    {
+      remainder = 1;
+      test -= range;
+    }
+    next[length - i - 1] = test;
+  }
+  return remainder;
+  // if (remainder != 0)
+  // {
+  // break;
+  // }
+  // else
+  // {
+  // uint8_t computed_hash[20];
+  // uint8_t reduced[length];
+  //
+  // printf("hash = ");
+  // hash(next, length, computed_hash);
+  // for (int i = 0; i < 20; i++)
+  // printf("%02x", computed_hash[i]);
+  // printf("\n");
+  //
+  // printf("reduction = \"");
+  // reduction(computed_hash, 20, reduced, length);
+  // for (int i = 0; i < length; i++)
+  // printf("%c", (int)reduced[i]);
+  // printf("\"\n");
+  // }
+}
+
 int main()
 {
   const char *start = "     ";
-  int step = 20000000;
   // 32 - 126 = 94; podzielić na 94ki
   int length = strlen(start);
   char *begining = malloc((length + 1) * sizeof(*begining));
   memcpy(begining, start, length);
   char *next = malloc((length + 1) * sizeof(*next));
   next[length] = 0;
+  int step = 20000000;
   int min = 32;
   int max = 126;
   int range = 95;
@@ -55,8 +105,6 @@ int main()
       char this_step = tmp / ipow(range, i);
       this_step += remainder;
       remainder = 0;
-      // printf("DEBUG: begining =\"%s\"\n", begining);
-      // printf("DEBUG: next =\"%s\"\n", next);
       char test = begining[length - i - 1] + this_step;
       // potensiall overflow problem
       //Max step is also the bigest possible value in one column
@@ -65,13 +113,10 @@ int main()
         remainder = 1;
         test -= range;
       }
-      // }
-
       next[length - i - 1] = test;
     }
     if (remainder != 0)
     {
-      printf("after loop non zero remainder\n");
       break;
     }
     else
@@ -93,19 +138,6 @@ int main()
     }
     // printf("begining =\"%s\"\n", begining);
     printf("next      =\"%s\"\n", next);
-    // printf("next(char)=");
-    // for (int i = 0; i < length; i++)
-    // {
-    // printf("%2c|", next[i]);
-    // }
-    // printf("\n");
-    // printf("next(int)= ");
-    // for (int i = 0; i < length + 1; i++)
-    // {
-    // printf("%2d|", next[i]);
-    // }
-    // printf("\n");
-    // printf("sizeof char* = %ld\nsizeof uint64_t %zu\n", sizeof(char *), sizeof(uint64_t));
     SWAP(next, begining);
   }
   printf("start     =\"%s\"\n", start);
@@ -118,6 +150,8 @@ int main()
   // }
   // printf("\"\n");
   // printf("next = %s\n", next);
+  free(begining);
+  free(next);
 
   return 0;
 }
