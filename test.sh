48446a131898743499d6cba36901d9f7bd90ada3
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-06-12 15:17:16 +0200

    Update to time testing script

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-time mpiexec -n 1 bin/rb.out > /dev/null;
-time mpiexec -n 2 bin/rb.out > /dev/null;
-time mpiexec -n 3 bin/rb.out > /dev/null;
-time mpiexec -n 4 bin/rb.out > /dev/null;
+export TIMEFMT=$'\n================\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
+time mpiexec -n 1 bin/rb.out 2> time_trials.txt
+time mpiexec -n 2 bin/rb.out 2>> time_trials.txt
+time mpiexec -n 3 --oversubscribe bin/rb.out 2>> time_trials.txt
+time mpiexec -n 4 --oversubscribe bin/rb.out 2>> time_trials.txt
