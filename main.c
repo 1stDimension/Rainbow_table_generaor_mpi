e1b3110b254f4d7424ba7185623977cb97fbdccb
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-06-12 23:13:18 +0200

    Adding changing of output file name

--- main.c
+++ main.c
@@ -18,10 +18,6 @@
     a = b;         \
     b = t;         \
   }
-// hash_value
-// new_hash_value
-// reduce_value
-// new_reduce_value
 int ipow(int base, int exp)
 {
   int result = 1;
@@ -41,24 +37,12 @@ int chain(int chain_length, char *input, int password_length, char *output, int 
 {
   uint8_t reduced[password_length];
 
-  // printf("hash = ");
   hash(input, password_length, output);
-  // for (int i = 0; i < 20; i++)
-  // printf("%02x", (uint8_t)output[i]);
-  // printf("\n");
   chain_length--;
   for (int j = 0; j < chain_length; j++)
   {
-    // printf("reduction %3d = \"", j);
     reduction(output, HASH_LENGTH, reduced, password_length);
-    // for (int i = 0; i < password_length; i++)
-    // printf("%c", (int)reduced[i]);
-    // printf("\"\n");
-    // printf("hash = %3d", j);
     hash(reduced, password_length, output);
-    // for (int i = 0; i < 20; i++)
-    // printf("%02x", (uint8_t)output[i]);
-    // printf("\n");
   }
 }
 
@@ -87,33 +71,69 @@ int get_next(int step, int min, int max, int range, int length, int remainder, c
   return remainder;
 }
 
-int handle_input()
+int handle_input(int argc, char **argv, int *length_ptr, int *link_length_ptr, int *step_ptr, char **outputfile)
 {
-
-  int length = 5;
-  int step = 6000;
-  int min = 32;
-  int max = 126;
-  int range = 95;
-  int remainder = 0;
-  int max_step = range - 1;
-  int num_passwords;
+  int wrong = 0;
+  if (argc < 2)
+  {
+    printf("Password length must be specified\n");
+    wrong = 1;
+  }
+  else
+  {
+    *length_ptr = atoi(argv[1]);
+    if (*length_ptr == 0)
+    {
+      printf("Wrong value passed as password length\n");
+      wrong = 1;
+    }
+  }
+  if (argc > 2)
+  {
+    printf("2\n");
+    *link_length_ptr = atoi(argv[2]);
+    if (*link_length_ptr == 0)
+    {
+      printf("Wrong value passed as link length\n");
+      wrong = 2;
+    }
+  }
+  if (argc > 3)
+  {
+    printf("3\n");
+    *step_ptr = atoi(argv[3]);
+    if (*step_ptr == 0)
+    {
+      printf("Wrong value passed as step size\n");
+      wrong = 3;
+    }
+  }
+  if (argc > 4)
+  {
+    int size = strlen(argv[4] + 1);
+    *outputfile = malloc((size + 1) * sizeof(**outputfile));
+    memcpy(*outputfile, argv[4], size);
+  }
+  else
+  {
+    char d[] = "output.txt";
+    int size = strlen(d);
+    *outputfile = malloc((size + 1) * sizeof(**outputfile));
+    (*outputfile)[size] = 0;
+    memcpy(*outputfile, d, size);
+  }
+  if (wrong != 0)
+  {
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    // MPI_Finalize();
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main(int argc, char **argv)
 {
   // (Always push back length of passwords times)
-  MPI_Init(&argc, &argv);
-  int world_size, rank;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  cvector_vector_type(char) all_possibilities = NULL;
-  cvector_vector_type(char) my_input = NULL;
-  cvector_vector_type(char) my_output = NULL;
-  cvector_vector_type(char) all_outputs = NULL;
-  const char *start = "     ";
-  // 32 - 126 = 94;
-  int length = strlen(start);
+  MPI_Init(NULL, NULL);
   int step = 6000;
   int min = 32;
   int max = 126;
@@ -122,6 +142,24 @@ int main(int argc, char **argv)
   int max_step = range - 1;
   int num_passwords;
   int link_length = 20;
+  int world_size, rank;
+  int length = 5;
+  char *file;
+  char *start;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == MASTER_ID)
+  {
+    handle_input(argc, argv, &length, &link_length, &step, &file);
+    printf("length = %d, link_length = %d, step = %d, outputfile = %s\n", length, link_length, step, file);
+    start = malloc((length + 1) * sizeof(*start));
+    memset(start, ' ', (length) * sizeof(*start));
+    start[length] = 0;
+  }
+  cvector_vector_type(char) all_possibilities = NULL;
+  cvector_vector_type(char) my_input = NULL;
+  cvector_vector_type(char) my_output = NULL;
+  cvector_vector_type(char) all_outputs = NULL;
   if (rank == MASTER_ID)
   {
     // +1 because adding null terminating string
@@ -145,7 +183,7 @@ int main(int argc, char **argv)
       char *this = all_possibilities + j * (length + 1);
       char *n = this + length + 1;
       overflow = get_next(step, min, max, range, length, remainder, this, n);
-      //adjust size because last element is not important if overflowhappened
+      //adjust size because last element is not important if overflow happened
       if (overflow != 0)
       {
         for (int k = 0; k < length + 1; k++)
@@ -160,21 +198,17 @@ int main(int argc, char **argv)
       }
     }
     num_passwords = cvector_size(all_possibilities) / (length + 1);
-    // print all passwords
-    // for (int i = 0; i < num_passwords; i++)
-    // {
-    // for (int j = 0; j < length; j++)
-    // {
-    // printf("%c",
-    //  all_possibilities[i * (length + 1) + j]);
-    // }
-    // printf("\n");
-    // }
   }
-
   MPI_Bcast(&num_passwords, 1, MPI_INT, MASTER_ID, MPI_COMM_WORLD);
   MPI_Bcast(&link_length, 1, MPI_INT, MASTER_ID, MPI_COMM_WORLD);
   MPI_Bcast(&length, 1, MPI_INT, MASTER_ID, MPI_COMM_WORLD);
+  MPI_Bcast(&step, 1, MPI_INT, MASTER_ID, MPI_COMM_WORLD);
+  if (rank != MASTER_ID)
+  {
+    start = malloc((length + 1) * sizeof(*start));
+    memset(start, ' ', (length) * sizeof(*start));
+    start[length] = 0;
+  }
   // Data type for passwords
   MPI_Datatype password_type;
   MPI_Type_contiguous(length + 1, MPI_CHAR, &password_type);
@@ -201,17 +235,6 @@ int main(int argc, char **argv)
     chain(link_length, my_input + i * (length + 1), length, my_output + i * HASH_LENGTH, HASH_LENGTH);
   }
 
-  // for (int i = 0; i < my_share; i++)
-  // {
-  // for (int j = 0; j < HASH_LENGTH; j++)
-  // {
-  // printf("%d|", (int)
-  // my_output[i * HASH_LENGTH + j]);
-  // }
-  // printf("\n");
-  // }
-
-  // MPI_Scatter(sizes, 1, MPI_INT, &mySize, 1, MPI_INT, master, MPI_COMM_WORLD);
   MPI_Gather(my_output, HASH_LENGTH * my_share, MPI_CHAR, all_outputs, HASH_LENGTH * my_share, MPI_CHAR, MASTER_ID, MPI_COMM_WORLD);
   cvector_free(my_output);
   cvector_free(my_input);
@@ -224,7 +247,6 @@ int main(int argc, char **argv)
       chain(link_length, all_possibilities + i * (length + 1), length, all_outputs + i * HASH_LENGTH, HASH_LENGTH);
     }
 
-    char *file = "output.txt";
     FILE *w = fopen(file, "w");
     printf("Saving to file\n");
     for (int i = 0; i < num_passwords; i++)
@@ -241,6 +263,8 @@ int main(int argc, char **argv)
 
   if (rank == MASTER_ID)
   {
+    free(start);
+    free(file);
     cvector_free(all_possibilities);
     cvector_free(all_outputs);
   }
